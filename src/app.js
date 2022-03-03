059dff6d1945b8d8f1d82a307e6af4a449a94395
Rishabh Nagar <56214878+Rishabh1106@users.noreply.github.com>
2022-03-03 19:28:32 +0530

    Update port to 8080

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
